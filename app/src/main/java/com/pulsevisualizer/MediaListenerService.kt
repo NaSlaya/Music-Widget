6efc5989f48b2807e384a06e621e76509207a255
NaSlaya <wrightmax119@gmail.com>
2026-08-16 01:49:14 +1000

    Update MediaListenerService.kt

--- app/src/main/java/com/pulsevisualizer/MediaListenerService.kt
+++ app/src/main/java/com/pulsevisualizer/MediaListenerService.kt
@@ -1,27 +1,16 @@
 package com.pulsevisualizer
 
-import android.content.ComponentName
 import android.service.notification.NotificationListenerService
 
 class MediaListenerService : NotificationListenerService() {
 
     override fun onListenerConnected() {
         super.onListenerConnected()
-
-        val listenerComponent = ComponentName(
-            this,
-            MediaListenerService::class.java
-        )
-
-        MediaRepository.start(
-            context = this,
-            notificationListenerComponent = listenerComponent
-        )
+        MediaRepository.start(this)
     }
 
     override fun onListenerDisconnected() {
         MediaRepository.stop()
-
         super.onListenerDisconnected()
     }
 }
